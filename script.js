185a5b11b1c39fdabc9261cf25c1a72a416dad82
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-09-07 20:38:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your code here
+
+const namee = document.getElementById('item-name-input');
+const price = document.getElementById('item-price-input');
+const add = document.getElementById('add').addEventListener('click', addElement);
+const tbody = document.getElementById('tbody');
+const total = document.getElementById('total');
+
+
+function addElement() {
+    // console.log(namee.value, price.value)
+    const row = document.createElement('tr');
+    const td1 = document.createElement('td');
+    td1.innerText = namee.value;
+
+    const td2 = document.createElement('td');
+    td2.innerText = price.value;
+
+    row.appendChild(td1);
+    row.appendChild(td2);
+    //  console.log(tbody.children[0].children[1].innerText)
+    tbody.appendChild(row);
+
+    let sum = 0;
+    for(let i=0; i<tbody.children.length; i++){
+        sum += Number(tbody.children[i].children[1].innerText);
+    }
+
+    total.innerText = sum;
+    namee.value = '';
+    price.value = '';
+}
